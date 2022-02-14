e3cc9a39119efce34952c1298d9b29d7ea719077
mjclinic <20180817@vision.hoseo.edu>
2022-02-14 21:42:53 +0900

    debug ball

--- boot.asm
+++ boot.asm
@@ -1,14 +1,28 @@
 bits 16
 org 0x7c00
 
+    mov ax, 0xb800
+    mov es, ax
 
-game_start:
     mov ah, 0x00
     mov al, 0x03
     int 0x10
 
-    mov ax, 0xb800
-    mov es, ax
+    mov ah, 1
+    mov ch, 0x26
+    mov cl, 0x07
+    int 0x10
+
+    mov ah, 2
+    mov dh, 24
+    mov dl, 79
+    int 0x10
+
+game_start:
+
+
+
+    
     .init:
     mov di, 0
     mov si, 0
@@ -18,12 +32,41 @@ game_start:
     mov di, 0
     .bg_check:
     cmp di, 2*80*25-1
-    jge .line1
+    jg .keyboard
     mov si, 0
     mov dl, 0B_1111_0000
     call print
     add di, 2
     jmp .bg_check
+
+  
+    .keyboard:
+    mov ah, 0x01
+    int 0x16
+
+    je .line1 ; je= 1
+
+    mov ah, 0x00
+    int 0x16
+    cmp al, 0x6A
+    je .jump
+    jmp .keyboard2
+
+    .jump:
+    add word [padle_l], 1
+
+    call padle
+
+    .keyboard2:
+    cmp al, 0x6B
+    je .kick
+    jmp .line1
+
+    .kick:
+    sub word [padle_l], 1
+
+    call padle
+
 
     .line1:
     mov di, 160
@@ -31,7 +74,7 @@ game_start:
     cmp di, 2*80*24-1
     jge .line_end
     mov si, 0
-    mov dl, 0B_0000_0000
+    mov dl, 0B_0111_0000
     call print
     add di, 2
     jmp .line_check
@@ -49,31 +92,7 @@ game_start:
     add bx, 160*4
     mov di, [padle_r]
     call padle
-    
-
-    .callre:
-    cmp word[ball_x], 1
-    jne .done
-
-    mov ax, word[ball_y]
-    mov bx, word[padle_l]
-    cmp ax, bx
-    jl .done 
-
-    add bx, 4
-    cmp ax, bx
-    jg .done
-
-    neg word[ball_vx]
-    
-    .done:
-    mov di, [ball_x]
-    mov si, [ball_y]
-    call ball
-    mov di,ax
-    mov si, "B"
-    mov dl, 0b_0000_1101
-    call print
+ 
 
     mov ax , [ball_vx]
     add word [ball_x], ax
@@ -96,45 +115,47 @@ game_start:
     mov ax , [ball_b]
     cmp word [ball_y], ax
     je .inversey
-    jmp .keyboard
+    jmp .callre
     .inversey:
     neg word [ball_vy]
+
+
+
+
+
+   
+
+    .callre:
+    cmp word[ball_x], 1
+    jne .done
+
+    mov ax, word[ball_y]
+    mov bx, word[padle_l]
+    cmp ax, bx
+    jl .done 
+
+    add bx, 4
+    cmp ax, bx
+    jg .done
+
+    neg word[ball_vx]
     
-  
-    .keyboard:
-    mov ah, 0x01
-    int 0x16
-
-    je .end ; je= 1
-
-    mov ah, 0x00
-    int 0x16
-    cmp al, 0x6A
-    je .jump
-    jmp .keyboard2
-
-    .jump:
-    add word [padle_l], 1
-
-    call padle
-
-    .keyboard2:
-    cmp al, 0x6B
-    je .kick
-    jmp .end
-
-    .kick:
-    sub word [padle_l], 1
-
-    call padle
-
-
 
     
+
+
+    .done:
+    mov di, [ball_x]
+    mov si, [ball_y]
+    call ball
+    mov di,ax
+    mov si, "B"
+    mov dl, 0b_0000_1101
+    call print
 
     .end:
     mov bx, [0x_046c]
-    add bx, 1
+    add bx, 2
     .delay:
     cmp bx, [0x_046c]
     jge .delay
@@ -142,8 +163,8 @@ game_start:
 
 ball:
 ;di: x si: y
-imul di, di,2
-imul si, si,160
+imul di, [ball_x],2
+imul si, [ball_y],160
 add di,si
 mov ax,di
 ret
